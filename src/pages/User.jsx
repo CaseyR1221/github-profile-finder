9964642dd426407bc1dcf14318c29aa38cb25c05
Casey R <crowlandsdev@gmail.com>
2023-12-22 22:41:10 -0500

    cleaned up files

--- src/pages/User.jsx
+++ src/pages/User.jsx
@@ -3,16 +3,24 @@ import GithubContext from '../context/github/GithubContext'
 import { useParams } from 'react-router-dom'
 import { FaCodepen, FaStore, FaUserFriends, FaUsers} from 'react-icons/fa'
 import { Link } from 'react-router-dom'
+import RepoList from '../components/repos/RepoList'
 import Spinner from '../components/layout/Spinner'
+import { getUserAndRepos } from '../context/github/GithubActions'
 
 const User = () => {
-  const {getUser, user, loading} = useContext(GithubContext)
+  const {user, repos, loading, dispatch} = useContext(GithubContext)
 
   const params = useParams()
 
   useEffect(() => {
-    getUser(params.login)
-  }, [])
+    dispatch({ type: 'SET_LOADING' })
+    const getUserData = async () => {
+      const userData = await getUserAndRepos(params.login)
+      dispatch({ type: 'GET_USER_AND_REPOS', payload: userData })
+    }
+
+    getUserData()
+  }, [dispatch, params.login])
 
   const {
     name,
@@ -160,6 +168,8 @@ const User = () => {
             </div>
           </div>
         </div>
+
+        <RepoList repos={repos} />
       </div>
     </>
   )
